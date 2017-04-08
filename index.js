17492116f923e48a28f2f4cbea05de306c88c62c
Colin <colin.diesh@gmail.com>
2017-04-07 21:02:40 -0400

    Update deps

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-var vg = require('vega')
+var vg = require('vega');
+var vl = require('vega-lite');
 // Assign the specification to a local variable vlSpec.
 var vlSpec = {
-  "data": {
-    "values": [
-      {"a": "C", "b": 2}, {"a": "C", "b": 7}, {"a": "C", "b": 4},
-      {"a": "D", "b": 1}, {"a": "D", "b": 2}, {"a": "D", "b": 6},
-      {"a": "E", "b": 8}, {"a": "E", "b": 4}, {"a": "E", "b": 7}
-    ]
-  },
-  "mark": "bar",
+  "description": "A scatterplot showing horsepower and miles per gallons for various cars.",
+  "data": {"url": "data/cars.json"},
+  "mark": "point",
   "encoding": {
-    "y": {"field": "a", "type": "nominal"},
-    "x": {
-      "aggregate": "average", "field": "b", "type": "quantitative",
-      "axis": {
-        "title": "Average of b"
-      }
-    }
+    "x": {"field": "Horsepower","type": "quantitative"},
+    "y": {"field": "Miles_per_Gallon","type": "quantitative"}
   }
-};
+}
 
-var embedSpec = {
-  mode: "vega-lite",  // Instruct Vega-Embed to use the Vega-Lite compiler
-  spec: vlSpec
-  // You can add more vega-embed configuration properties here.
-  // See https://github.com/vega/vega/wiki/Embed-Vega-Web-Components#configuration-propeties for more information.
-};
+//var embedSpec = {
+//  mode: "vega-lite",  // Instruct Vega-Embed to use the Vega-Lite compiler
+//  spec: vlSpec
+//  // You can add more vega-embed configuration properties here.
+//  // See https://github.com/vega/vega/wiki/Embed-Vega-Web-Components#configuration-propeties for more information.
+//};
+//
+//// Embed the visualization in the container with id `vis`
+//vg.embed("#vis", embedSpec, function(error, result) {
+//  // Callback receiving the View instance and parsed Vega spec
+//  // result.view is the View, which resides under the '#vis' element
+//});
+//
+//
 
-// Embed the visualization in the container with id `vis`
-vg.embed("#vis", embedSpec, function(error, result) {
-  // Callback receiving the View instance and parsed Vega spec
-  // result.view is the View, which resides under the '#vis' element
+var vgSpec = vl.compile(vlSpec).spec;
+console.log(vgSpec)
+
+// parse a spec and create a visualization view
+vg.parse.spec(vgSpec, (chart) => {
+    var view = chart({ el: 'vis' }).update();
+
+    if (animate) {
+        updategraph(view, csvdata, 1);
+    }
 });
