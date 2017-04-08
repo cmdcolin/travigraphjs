db8724024e1d823e5ab72f89cfd3da0f8458afd0
Colin <colin.diesh@gmail.com>
2017-04-08 12:31:42 -0400

    Eslint

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* global vega, vl */
+
 
 //stackoverflow
 function getParameterByName(name) {
@@ -9,7 +11,7 @@ function filterOutliers(someArray) {
     var values = someArray.concat();
     values.sort( function(a, b) {
         return a.duration - b.duration;
-     });
+    });
 
     var q1 = values[Math.floor((values.length / 4))].duration;
     var q3 = values[Math.ceil((values.length * (3 / 4)))].duration;
@@ -28,32 +30,42 @@ function filterOutliers(someArray) {
 // create spec
 function process(data) {
     Promise.all(data).then(function(total_builds) {
-        var json = total_builds.map(function(m) { return m.json(); })
+        var json = total_builds.map(function(m) { return m.json(); });
         Promise.all(json).then(function(ret) {
             var d = Array.prototype.concat.apply([], ret);
-            d.map(function(r) { if(r.result==0) r.state="succeeded"; })
-            d.map(function(r) { if(r.result==1) r.state="failed"; })
-            d.map(function(r) { if(r.result==null) r.state="errored"; })
-            d.map(function(r) { r.duration/=60; })
+            if(!d.length) {
+                document.querySelector('#view').innerHTML = '<div class="alert alert-warning">Error: no results found for repository</div>';
+                return;
+            }
+
+            d.map(function(r) { if(r.result==0) r.state='succeeded'; });
+            d.map(function(r) { if(r.result==1) r.state='failed'; });
+            d.map(function(r) { if(r.result==null) r.state='errored'; });
+            d.map(function(r) { r.duration/=60; });
             d=filterOutliers(d);
             var spec = {
-              "description": "Travis-CI builds",
-              "data": { values: d },
-              "mark": "point",
-              "encoding": {
-                "y": {"field": "duration","type": "quantitative", "axis": {"title": "Duration (minutes)"} },
-                "x": {"field": "finished_at","type": "temporal", "axis": {"title": "Date"} },
-                "color": {"field": "state", "type": "nominal", "scale": {"range": ["#d62728", "#ff7f0e", "#1f77b4"]}},
-              },
-              "width": 1000,
-              "height": 400
-            }
+                description: 'Travis-CI builds',
+                data: { values: d },
+                mark: 'point',
+                encoding: {
+                    y: {field: 'duration',type: 'quantitative', axis: {title: 'Duration (minutes)'} },
+                    x: {field: 'finished_at',type: 'temporal', axis: {title: 'Date'} },
+                    color: {field: 'state', type: 'nominal', scale: {range: ['#d62728', '#ff7f0e', '#1f77b4']}},
+                },
+                width: 1000,
+                height: 400
+            };
 
 
             var vgSpec = vl.compile(spec).spec;
             var runtime = vega.parse(vgSpec);
-            var view = new vega.View(runtime).initialize(document.querySelector('#view')).run();
+            var view = new vega.View(runtime);
+            view.initialize(document.querySelector('#view')).run();
+        }, function(error) {
+            document.querySelector('#view').innerHTML = '<div class="alert alert-warning">'+error+'</div>';
         });
+    }, function(error) {
+        document.querySelector('#view').innerHTML = '<div class="alert alert-warning">'+error+'</div>';
     });
 }
 function graph(e) {
@@ -62,7 +74,7 @@ function graph(e) {
     var end = document.getElementById('end').value;
     document.getElementById('view').innerHTML = 'Loading...';
 
-    window.history.replaceState({}, "", '?repo='+repo+'&start='+start+'&end='+end);
+    window.history.replaceState({}, '', '?repo='+repo+'&start='+start+'&end='+end);
 
 
     var data = [];
@@ -71,11 +83,11 @@ function graph(e) {
     var fetcher = function() {
         if(iter < +end) {
             var builds = fetch('https://api.travis-ci.org/repos/' + repo + '/builds?after_number=' + iter);
-            data.push(builds)
+            data.push(builds);
             document.getElementById('view').innerHTML = 'Loading build '+iter+'...';
         } else {
             clearInterval(intervalId);
-            process(data)
+            process(data);
         }
         iter += 25;
     };
@@ -88,9 +100,9 @@ document.querySelector('form').addEventListener('submit',graph);
 
 var ret = getParameterByName('repo');
 if(ret) document.getElementById('repo').value = ret;
-var ret = getParameterByName('start');
+ret = getParameterByName('start');
 if(ret) document.getElementById('start').value = ret;
-var ret = getParameterByName('end');
+ret = getParameterByName('end');
 if(ret) document.getElementById('end').value = ret;
 graph();
 
