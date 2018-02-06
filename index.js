936d80fff2683edab7bdb9a33b92730d27102bcb
Colin <colin.diesh@gmail.com>
2018-02-06 12:16:16 -0500

    Fix eslint warnings

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* global vega, vl, vegaTooltip, vegaEmbed */
+/* global vegaTooltip, vegaEmbed */
 
 
 // stackoverflow
@@ -15,8 +15,8 @@ function filterOutliers(someArray) {
     const q3 = values[Math.ceil((values.length * (3 / 4)))].duration;
     const iqr = q3 - q1;
 
-    const maxValue = q3 + iqr * 3;
-    const minValue = q1 - iqr * 3;
+    const maxValue = q3 + (iqr * 3);
+    const minValue = q1 - (iqr * 3);
 
     const filteredValues = values.filter(x => (x.duration < maxValue) && (x.duration > minValue));
 
@@ -25,8 +25,8 @@ function filterOutliers(someArray) {
 
 // create spec
 function process(data) {
-    Promise.all(data).then((total_builds) => {
-        const json = total_builds.map(m => m.json());
+    Promise.all(data).then((totalBuilds) => {
+        const json = totalBuilds.map(m => m.json());
         Promise.all(json).then((ret) => {
             let d = Array.prototype.concat.apply([], ret);
             if (!d.length) {
@@ -34,10 +34,10 @@ function process(data) {
                 return;
             }
 
-            d.map((r) => { if (r.result == 0) r.state = 'succeeded'; });
-            d.map((r) => { if (r.result == 1) r.state = 'failed'; });
-            d.map((r) => { if (r.result == null) r.state = 'errored'; });
-            d.map((r) => { r.duration /= 60; });
+            d.forEach((r) => { if (r.result === 0) r.state = 'succeeded'; });
+            d.forEach((r) => { if (r.result === 1) r.state = 'failed'; });
+            d.forEach((r) => { if (r.result === null) r.state = 'errored'; });
+            d.forEach((r) => { r.duration /= 60; });
             d = filterOutliers(d);
             const spec = {
                 description: 'Travis-CI builds',
@@ -56,7 +56,6 @@ function process(data) {
                 mode: 'vega-lite',
             };
             vegaEmbed('#view', spec, opt).then((result) => {
-                console.log(result);
                 vegaTooltip.vegaLite(result.view, spec);
             }).catch((error) => {
                 document.querySelector('#view').innerHTML = `<div class="alert alert-warning">${error}</div>`;
@@ -79,8 +78,7 @@ function graph(e) {
 
     const data = [];
     let iter = +start;
-    let intervalId = null;
-    const fetcher = function () {
+    const intervalId = setInterval(() => {
         if (iter < +end) {
             const builds = fetch(`https://api.travis-ci.org/repos/${repo}/builds?after_number=${iter}`);
             data.push(builds);
@@ -90,17 +88,16 @@ function graph(e) {
             process(data);
         }
         iter += 25;
-    };
-
-    intervalId = setInterval(fetcher, 200);
-    e && e.preventDefault();
+    }, 200);
+    if (e) {
+        e.preventDefault();
+    }
 }
 
 document.querySelector('form').addEventListener('submit', graph);
 
-const f = getParameterByName;
-if (f('repo')) document.getElementById('repo').value = f('repo');
-if (f('start')) document.getElementById('start').value = f('start');
-if (f('end')) document.getElementById('end').value = f('end');
+if (getParameterByName('repo')) document.getElementById('repo').value = getParameterByName('repo');
+if (getParameterByName('start')) document.getElementById('start').value = getParameterByName('start');
+if (getParameterByName('end')) document.getElementById('end').value = getParameterByName('end');
 graph();
 
