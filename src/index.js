2f64e03a5930f35a5a7e4527b4d094d00e8ca873
Colin <colin.diesh@gmail.com>
2018-04-09 22:17:16 -0400

    Fix eslint warnings

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-/* global vegaTooltip, vegaEmbed */
+/* global vegaTooltip, vegaEmbed, vega, vl */
 import '@babel/polyfill';
 
 
-// stackoverflow
-function getParameterByName(name) {
-    const match = RegExp(`[?&]${name}=([^&]*)`).exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
 // stackoverflow
 function filterOutliers(someArray) {
     const values = someArray.concat();
@@ -25,62 +20,57 @@ function filterOutliers(someArray) {
 }
 
 // create spec
-function process(data) {
-    Promise.all(data).then((requests) => {
-        Promise.all(requests.map(m => m.json())).then((ret) => {
-            let d = [].concat(...(ret.map((m) => m.builds)));
-            d = d.map((m) => {
-                return {
-                    message: m.commit.message,
-                    branch: (m.branch||{}).name,
-                    duration: m.duration,
-                    number: m.number,
-                    finished_at: m.finished_at,
-                    state: m.state
-                }
-            });
-            if (!d.length) {
-                document.querySelector('#view').innerHTML = '<div class="alert alert-warning">Error: no results found for repository</div>';
-                return;
-            }
+async function process(data) {
+    const requests = await Promise.all(data);
+    const ret = await requests.map(m => m.json());
+    let d = [].concat(...(ret.map(m => m.builds)));
+    d = d.map(m => ({
+        message: m.commit.message,
+        branch: (m.branch || {}).name,
+        duration: m.duration,
+        number: m.number,
+        finished_at: m.finished_at,
+        state: m.state,
+    }));
+    if (!d.length) {
+        document.querySelector('#view').innerHTML = '<div class="alert alert-warning">Error: no results found for repository</div>';
+        return;
+    }
 
-            d.forEach((r) => { r.duration /= 60; });
-            d = filterOutliers(d);
-            const spec = {
-                description: 'Travis-CI builds',
-                data: { values: d },
-                mark: 'point',
-                encoding: {
-                    y: { field: 'duration', type: 'quantitative', axis: { title: 'Duration (minutes)' } },
-                    x: { field: 'finished_at', type: 'temporal', axis: { title: 'Date' } },
-                    color: { field: 'state', type: 'nominal', scale: { domain: ['failed', 'errored', 'cancelled', 'passed'], range: ['#d62728', '#ff7f0e', '#5ab43c', '#1f77b4'] } },
+    for (let i = 0; i < d.length; i += 1) {
+        d[i].duration /= 60;
+    }
+    d = filterOutliers(d);
+    const spec = {
+        description: 'Travis-CI builds',
+        data: { values: d },
+        mark: 'point',
+        encoding: {
+            y: { field: 'duration', type: 'quantitative', axis: { title: 'Duration (minutes)' } },
+            x: { field: 'finished_at', type: 'temporal', axis: { title: 'Date' } },
+            color: {
+                field: 'state',
+                type: 'nominal',
+                scale: {
+                    domain: ['failed', 'errored', 'cancelled', 'passed'],
+                    range: ['#d62728', '#ff7f0e', '#5ab43c', '#1f77b4'],
                 },
-                width: 1000,
-                height: 400,
-            };
+            },
+        },
+        width: 1000,
+        height: 400,
+    };
 
-            const opt = {
-                mode: 'vega-lite',
-            };
-            vegaEmbed('#view', spec, opt).then((result) => {
-                vegaTooltip.vegaLite(result.view, spec);
-            }).catch((error) => {
-                document.querySelector('#view').innerHTML = `<div class="alert alert-warning">${error}</div>`;
-            });
-        }, (error) => {
-            document.querySelector('#view').innerHTML = `<div class="alert alert-warning">${error}</div>`;
-        });
-    }, (error) => {
-        document.querySelector('#view').innerHTML = `<div class="alert alert-warning">${error}</div>`;
+    vegaEmbed('#view', spec, { mode: 'vega-lite' }).then((result) => {
+        vegaTooltip.vegaLite(result.view, spec);
     });
 }
 
-function timer(ms){
-     return new Promise(r=>setTimeout(r,ms));
+function timer(ms) {
+    return new Promise(r => setTimeout(r, ms));
 }
 
 async function graph(e) {
-
     if (e) {
         e.preventDefault();
     }
@@ -93,36 +83,34 @@ async function graph(e) {
 
 
     const data = [];
-    let limit = 100;
-    let headers = new Headers({ 'Travis-API-Version': '3' });
-    let res = await fetch(`https://api.travis-ci.org/repo/${repo}/builds?sort_by=id:desc`, { headers: headers });
-    if(res.status == "404") {
-        document.querySelector('#view').innerHTML = `<div class="alert alert-warning">Repo not found</div>`;
+    const limit = 100;
+    const headers = new Headers({ 'Travis-API-Version': '3' });
+    const res = await fetch(`https://api.travis-ci.org/repo/${repo}/builds?sort_by=id:desc`, { headers });
+    if (res.status === '404') {
+        document.querySelector('#view').innerHTML = '<div class="alert alert-warning">Repo not found</div>';
         return;
-    }
-    else if(res.status != "200") {
+    } else if (res.status !== '200') {
         document.querySelector('#view').innerHTML = `<div class="alert alert-warning">Error ${res.status}: ${res.statusText}</div>`;
         return;
     }
-    let resjs = await res.json();
-    let totalBuilds = document.getElementById('end').value;
+    const resjs = await res.json();
     document.querySelector('#totalBuilds').innerHTML = `Total number of builds: ${resjs.builds[0].number}`;
-    for(var i = +start; i <= +end; i+=limit) {
-        var builds = fetch(`https://api.travis-ci.org/repo/${repo}/builds?limit=${limit}&offset=${i}&sort_by=id`, { headers: headers });
+    for (let i = +start; i <= +end; i += limit) {
+        const builds = fetch(`https://api.travis-ci.org/repo/${repo}/builds?limit=${limit}&offset=${i}&sort_by=id`, { headers });
         data.push(builds);
         document.getElementById('view').innerHTML = `Loading build ${i}...`;
-        await timer(200);
+        await timer(500);
     }
     process(data);
-
 }
 
 document.querySelector('form').addEventListener('submit', graph);
 
-if (getParameterByName('repo')) document.getElementById('repo').value = getParameterByName('repo');
-if (getParameterByName('start')) document.getElementById('start').value = getParameterByName('start');
-if (getParameterByName('end')) document.getElementById('end').value = getParameterByName('end');
+const params = new URLSearchParams(window.location.search.slice(1));
+if (params.get('repo')) document.getElementById('repo').value = params.get('repo');
+if (params.get('start')) document.getElementById('start').value = params.get('start');
+if (params.get('end')) document.getElementById('end').value = params.get('end');
 graph();
 
 
-document.getElementById('versions').innerHTML += 'vega: ' + vega.version + ' vega-lite: ' + vl.version;
+document.getElementById('versions').innerHTML += `vega: ${vega.version} vega-lite: ${vl.version}`;
