a1a7ce9780904162ef8c3cba0fd6c6c3f6a8d596
Colin <colin.diesh@gmail.com>
2019-05-05 02:35:57 -0400

    Reactify everything

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import ReactDOM from 'react-dom'
 import React, { useState, useEffect } from 'react'
 import { useQueryParams, StringParam, NumberParam, ArrayParam } from 'use-query-params'
 import pLimit from 'p-limit'
-import BarChart from './barChart'
+import { createClassFromLiteSpec } from 'react-vega-lite'
 
 const headers = new Headers({ 'Travis-API-Version': '3' })
 const BUILDS_PER_REQUEST = 25
 
-const barData = {
-  values: [
-    { a: 'A', b: 20 },
-    { a: 'B', b: 34 },
-    { a: 'C', b: 55 },
-    { a: 'D', b: 19 },
-    { a: 'E', b: 40 },
-    { a: 'F', b: 34 },
-    { a: 'G', b: 91 },
-    { a: 'H', b: 78 },
-    { a: 'I', b: 25 },
-  ],
-}
+const LineChart = createClassFromLiteSpec('LineChart', {
+  $schema: 'https://vega.github.io/schema/vega-lite/v2.json',
+
+  width: 1000,
+  height: 400,
+  mark: 'point',
+  selection: {
+    grid: {
+      type: 'interval',
+      bind: 'scales',
+    },
+  },
+  encoding: {
+    y: {
+      field: 'duration',
+      type: 'quantitative',
+      axis: {
+        title: 'Duration (minutes)',
+      },
+    },
+    x: {
+      field: 'finished_at',
+      type: 'temporal',
+      axis: {
+        title: 'Date',
+      },
+    },
+    color: {
+      field: 'state',
+      type: 'nominal',
+      scale: {
+        domain: ['failed', 'errored', 'canceled', 'passed'],
+        range: ['#d62728', '#ff7f0e', '#5ab43c', '#1f77b4'],
+      },
+    },
+  },
+})
 
 function RepoForm(props) {
   const { onSubmit, init } = props
@@ -40,48 +64,69 @@ function RepoForm(props) {
       <button type="submit">Submit</button>
     </form>
   )
-  // return (
-  //   <>
-  //     <form
-  //       onSubmit={evt => {
-  //       }}
-  //     >
-  //       <label htmlFor="repo">Repository name</label>
-  //       <input id="repo" type="text" onChange={evt => setName(evt.target.value)} />
-  //       <label htmlFor="start">Start</label>
-  //       <input id="start" type="number" value={start} onChange={evt => setStart(evt.target.value)} />
-  //       <label htmlFor="end">End</label>
-  //       <input id="end" type="number" value={end} onChange={evt => setEnd(evt.target.value)} />
-  //       <button type="submit">Submit</button>
-  //     </form>
-  //   </>
-  // )
 }
 
 async function getBuilds({ repo, start, end }) {
-  const prefix = `https://api.travis-ci.org/repo/${repo}/builds`
+  const prefix = `https://api.travis-ci.org/repo/${encodeURIComponent(repo)}/builds?limit=${BUILDS_PER_REQUEST}`
   const input = []
   const limit = pLimit(1)
 
   for (let i = start; i <= end; i += BUILDS_PER_REQUEST) {
     input.push(
-      //limit(() => {
-      //return fetch(`${prefix}?limit=${BUILDS_PER_REQUEST}&offset=${i}&sort_by=id`, { headers })
-      `${prefix}?limit=${BUILDS_PER_REQUEST}&offset=${i}&sort_by=id`,
+      limit(() => {
+        return fetch(`${prefix}&offset=${i}&sort_by=id`, { headers })
+          .then(m => m.json())
+          .then(m => m.builds)
+      }),
     )
   }
-  console.log(input)
 
-  //   const result = await Promise.all(input)
-  //   const ret = await Promise.all(result.map(m => m.json()))
-  //   const builds = ret.map(m => m.builds)
-  //   return process([].concat(...builds))
-  return 'hello world'
+  const result = await Promise.all(input)
+  return result.flat()
+}
+
+// stackoverflow
+function filterOutliers(someArray) {
+  const values = someArray.concat()
+  values.sort((a, b) => a.duration - b.duration)
+
+  const q1 = values[Math.floor(values.length / 4)].duration
+  const q3 = values[Math.ceil(values.length * (3 / 4))].duration
+  const iqr = q3 - q1
+
+  const maxValue = q3 + iqr * 3
+  const minValue = q1 - iqr * 3
+
+  const filteredValues = values.filter(x => x.duration < maxValue && x.duration > minValue)
+
+  return filteredValues
+}
+
+function process(data) {
+  data = data.map(m => ({
+    message: (m.commit || {}).message,
+    branch: (m.branch || {}).name,
+    duration: m.duration,
+    number: m.number,
+    finished_at: m.finished_at,
+    state: m.state,
+  }))
+  if (!data.length) {
+    throw new Error('No results found for this repository')
+  }
+
+  for (let i = 0; i < data.length; i += 1) {
+    data[i].duration /= 60
+  }
+  return {
+    values: filterOutliers(data),
+  }
 }
 
 export default function App() {
-  const [downloadedRepoData, setDownloadedRepoData] = useState({ hits: [] })
+  const [downloadedRepoData, setDownloadedRepoData] = useState()
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
   const [query, setQuery] = useQueryParams({
     repo: StringParam,
     start: NumberParam,
@@ -90,31 +135,41 @@ export default function App() {
 
   const { repo, start, end } = query
   useEffect(() => {
+    if (repo && !repo.includes('/')) {
+      setError('Repo should be in the form user/name')
+    }
+  })
+  if (end <= start) {
+    setError('End should be greater than start')
+  }
+  useEffect(() => {
     async function getData(query) {
-      console.log('query', query)
       const result = await getBuilds(query)
-      console.log('here')
       setTimeout(() => {
         setLoading(false)
+        setDownloadedRepoData(process(result))
       }, 100)
-
-      //setDownloadedRepoData(result)
     }
     getData(query)
   }, [query])
 
+  const Error = err => {
+    console.log(err)
+    return <p style={{ color: 'red' }}>{err.error}</p>
+  }
+  console.log(downloadedRepoData)
   return (
     <>
       <RepoForm
         init={query}
         onSubmit={res => {
-          console.log('submitted', res)
           setLoading(true)
           setQuery(res)
         }}
       />
       {loading && <p>Loading...</p>}
-      {repo && !loading && <BarChart />}
+      {error && <Error error={error} />}
+      {downloadedRepoData && <LineChart data={downloadedRepoData} />}
     </>
   )
 }
