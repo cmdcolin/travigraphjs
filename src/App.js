804571bbbc6e61fef4e4caa3630a25c359af9a1b
Colin <colin.diesh@gmail.com>
2019-05-04 23:44:46 -0400

    Prettify and start adding form and hook for fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import BarChart from './barChart'
 
 const barData = {
@@ -15,10 +16,50 @@ const barData = {
     { a: 'I', b: 25 },
   ],
 }
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
 
-export default function App() {
-  return (
-    <BarChart data={barData} />
-  )
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    alert(`A name was submitted: ${this.state.value}`)
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
 }
 
+function App() {
+  const [data, setData] = useState({ hits: [] })
+
+  useEffect(async () => {
+    const result = await fetch('http://hn.algolia.com/api/v1/search?query=redux')
+
+    setData(result.data)
+  })
+
+  return (
+    <>
+      <NameForm onChange={evt => this.setData(evt.target.value)} />
+      <BarChart data={barData} />
+    </>
+  )
+}
