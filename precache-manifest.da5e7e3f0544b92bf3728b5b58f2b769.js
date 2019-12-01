0be409bfbfb68e8801343e2c57b3f2d76d096f5d
Colin <colin.diesh@gmail.com>
2019-12-01 13:17:51 -0500

    Create build

---
+++ precache-manifest.da5e7e3f0544b92bf3728b5b58f2b769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051177763c613191560f091fc22c02eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe0867cc71ce1ed2a171",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "58af9535e1b6d6991dd4",
+    "url": "/static/js/2.305e60bb.chunk.js"
+  },
+  {
+    "revision": "fe0867cc71ce1ed2a171",
+    "url": "/static/js/main.bbd3faff.chunk.js"
+  },
+  {
+    "revision": "d107feb7e0d0d61b169f",
+    "url": "/static/js/runtime-main.c648b37b.js"
+  }
+]);
