a0c2e75b18f6117cad73372cb8ba0025c5ab11a6
Colin <colin.diesh@gmail.com>
2019-12-03 11:33:24 -0500

    Update util

--- src/util.js
+++ src/util.js
@@ -5,7 +5,9 @@ export function filterOutliers(someArray = []) {
   values.sort((a, b) => a.duration - b.duration)
 
   const q1 = values[Math.floor(values.length / 4)].duration
-  const q3 = values[Math.ceil(values.length * (3 / 4))].duration
+  const q3 =
+    values[Math.min(Math.ceil(values.length * (3 / 4)), values.length - 1)]
+      .duration
   const iqr = q3 - q1
 
   const maxValue = q3 + iqr * 3
