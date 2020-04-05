774e7ab5fd03e76909262ad1f81a095f5483b833
Colin <colin.diesh@gmail.com>
2020-04-04 20:12:21 -0400

    Add build details

--- src/App.js
+++ src/App.js
@@ -14,90 +14,6 @@ import { filterOutliers, isAbortException } from './util'
 
 const BUILDS_PER_REQUEST = 100
 
-const spec = {
-  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-  width: 1000,
-  height: 400,
-  mark: { type: 'point', tooltip: { content: 'data' } },
-  data: { name: 'values' },
-  selection: {
-    grid: {
-      type: 'interval',
-      bind: 'scales',
-    },
-  },
-  encoding: {
-    y: {
-      field: 'duration',
-      type: 'quantitative',
-      axis: {
-        title: 'Duration (minutes)',
-      },
-    },
-    x: {
-      field: 'finished_at',
-      timeUnit: 'yearmonthdatehoursminutes',
-      type: 'temporal',
-      scale: {
-        nice: 'week', // add some padding/niceness to domain
-      },
-      axis: {
-        title: 'Date',
-      },
-    },
-    color: {
-      field: 'state',
-      type: 'nominal',
-      scale: {
-        domain: ['passed', 'failed', 'errored', 'canceled'],
-        range: ['#39aa56', '#ff7f0e', '#db4545', '#9d9d9d'],
-      },
-    },
-  },
-}
-
-const queueSpec = {
-  $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-  width: 1000,
-  height: 400,
-  mark: { type: 'point', tooltip: { content: 'data' } },
-  data: { name: 'values' },
-  selection: {
-    grid: {
-      type: 'interval',
-      bind: 'scales',
-    },
-  },
-  encoding: {
-    y: {
-      field: 'queue',
-      type: 'quantitative',
-      axis: {
-        title: 'Queue time (minutes)',
-      },
-    },
-    x: {
-      field: 'finished_at',
-      timeUnit: 'yearmonthdatehoursminutes',
-      type: 'temporal',
-      scale: {
-        nice: 'week', // add some padding/niceness to domain
-      },
-      axis: {
-        title: 'Date',
-      },
-    },
-    color: {
-      field: 'state',
-      type: 'nominal',
-      scale: {
-        domain: ['passed', 'failed', 'errored', 'canceled'],
-        range: ['#39aa56', '#ff7f0e', '#db4545', '#9d9d9d'],
-      },
-    },
-  },
-}
-
 const cache = new AbortablePromiseCache({
   cache: new QuickLRU({ maxSize: 1000 }),
   async fill(requestData, signal) {
@@ -115,13 +31,13 @@ const cache = new AbortablePromiseCache({
                 new Date(m.commit.committed_at)) /
               60000
             : 0
-        console.log(m)
         return {
           message: (m.commit || {}).message.slice(0, 20),
           branch: (m.branch || {}).name,
+          travisci_link: m.id,
           duration: m.duration / 60,
           queue: queue > 500 ? 0 : queue,
-          number: m.number,
+          build_number: m.number,
           commit_sha: m.commit.sha,
           compare: m.commit.compare_url,
           finished_at: m.finished_at,
@@ -214,6 +130,45 @@ function useTravisCI(signal, query) {
 
   return [loading, error, builds]
 }
+
+function BuildDetails({ build, repo, com }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(build).map(([key, value]) => {
+          if (key === 'travisci_link') {
+            const link = `http://travis-ci.${com}/${repo}/builds/${value}`
+
+            return (
+              <tr key={key + '_' + value}>
+                <td>{String(key)}</td>
+                <td>
+                  <a target="_blank" href={link}>
+                    {link}
+                  </a>
+                </td>
+              </tr>
+            )
+          } else {
+            return (
+              <tr key={key + '_' + value}>
+                <td>{String(key)}</td>
+                <td>{String(value)}</td>
+              </tr>
+            )
+          }
+        })}
+      </tbody>
+    </table>
+  )
+}
+
 export default function App() {
   const [controller, setController] = useState(new AbortController())
   const [query, setQuery] = useQueryParams({
@@ -226,6 +181,7 @@ export default function App() {
   })
 
   const [loading, error, builds] = useTravisCI(controller.signal, query)
+  const [clickedBuild, setClickedBuild] = useState(undefined)
   return (
     <>
       <h1>travigraph-js - Travis-CI duration graph</h1>
@@ -255,10 +211,78 @@ export default function App() {
       ) : loading ? (
         <p>{loading}</p>
       ) : (
-        <VegaLite
-          data={{ values: builds }}
-          spec={query.queue ? queueSpec : spec}
-        />
+        <div style={{ display: 'flex' }}>
+          <VegaLite
+            data={{ values: builds }}
+            patch={(spec) => {
+              spec.signals.push({
+                name: 'barClick',
+                value: 0,
+                on: [{ events: '*:mousedown', update: 'datum' }],
+              })
+              return spec
+            }}
+            signalListeners={{
+              barClick: (command, args) => {
+                setClickedBuild(args)
+              },
+            }}
+            spec={{
+              $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
+              width: 1000,
+              height: 400,
+              mark: { type: 'point', tooltip: { content: 'data' } },
+              data: { name: 'values' },
+              selection: {
+                grid: {
+                  type: 'interval',
+                  bind: 'scales',
+                },
+              },
+              encoding: {
+                y: query.queue
+                  ? {
+                      field: 'query',
+                      type: 'quantitative',
+                      axis: { title: 'Queue time(minutes)' },
+                    }
+                  : {
+                      field: 'duration',
+                      type: 'quantitative',
+                      axis: {
+                        title: 'Duration (minutes)',
+                      },
+                    },
+                x: {
+                  field: 'finished_at',
+                  timeUnit: 'yearmonthdatehoursminutes',
+                  type: 'temporal',
+                  scale: {
+                    nice: 'week', // add some padding/niceness to domain
+                  },
+                  axis: {
+                    title: 'Date',
+                  },
+                },
+                color: {
+                  field: 'state',
+                  type: 'nominal',
+                  scale: {
+                    domain: ['passed', 'failed', 'errored', 'canceled'],
+                    range: ['#39aa56', '#ff7f0e', '#db4545', '#9d9d9d'],
+                  },
+                },
+              },
+            }}
+          />
+          {clickedBuild ? (
+            <BuildDetails
+              repo={query.repo}
+              com={query.com ? 'com' : 'org'}
+              build={clickedBuild}
+            />
+          ) : null}
+        </div>
       )}
       <a href="https://github.com/cmdcolin/travigraphjs/">travigraph@GitHub</a>
     </>
